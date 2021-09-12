362b1beb2c45784eff2063327c6259663562eeaa
takanorip <takanori0170@gmail.com>
2021-09-12 21:49:32 +0900

    fix: external

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,14 @@
 import buble from '@rollup/plugin-buble';
 import pkg from './package.json';
 
+const external = Object.keys(pkg.dependencies);
+
 export default {
   input: 'src/index.js',
   plugins: [buble()],
   output: [
     { file: pkg.main, format: 'cjs', exports: 'auto' },
     { file: pkg.module, format: 'es' }
-  ]
+  ],
+  external
 };
